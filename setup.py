81f9104744eebf2b6a81d8631cbb47829fefdb75
Gabriel DE PERTHUIS <gabriel.de-perthuis@c-s.fr>
2009-07-23 14:52:03 +0000

    Modularity improvements.
    Move config, logging and such to common for the connector's benefit.
    Also rename vigilo-common-pubsub to vigilo-pubsub.
    Still need to take care of the vigilo.common.logging / vigilo.corr.libs
    layering issue.
    
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@361 b22e2e97-25c9-44ff-b637-2e5ceca36478

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 sw=4 ts=4 et :
+from distutils.core import setup
+
+setup(name='vigilo-common',
+        version='0.1',
+        author='Gabriel de Perthuis',
+        author_email='gabriel.de-perthuis@c-s.fr',
+        url='http://www.projet-vigilo.org/',
+        description='Common vigilo utilities',
+        license='http://www.gnu.org/licenses/gpl-2.0.html',
+        long_description='Common vigilo utilities\n'
+        +'Currently configuration, logging, python backward compatibility.\n',
+        install_requires=[
+            ],
+        namespace_packages = [
+            'vigilo',
+            ],
+        py_modules=[
+            'vigilo.common',
+            ],
+        entry_points={
+            'console_scripts': [
+                'vigilo-config = vigilo.common.conf:main',
+                ],
+            },
+        package_dir={'': 'src'},
+        )
+
---
+++ src/vigilo/common/conf.py
@@ -0,0 +1,78 @@
+# vim: set fileencoding=utf-8 sw=4 ts=4 et :
+from __future__ import absolute_import
+
+__all__ = ( 'settings', )
+
+"""
+A django-inspired settings module.
+
+Use dictionary access on the 'settings' object.
+Define VIGILO_SETTINGS_MODULE in the environment to a dotted module path
+that will be imported.
+The default is equivalent to 'import settings', meaning you should put
+a settings.py file in the current directory or the PYTHONPATH.
+
+Usage:
+    from vigilo.common.conf import settings
+    conntype = settings['MEMCACHE_CONN_TYPE']
+
+Command-line usage:
+    conntype=$(python -m vigilo.common.conf --get MEMCACHE_CONN_TYPE)
+
+"""
+
+"""
+Impl notes:
+    a ConfigParser.SafeConfigParser would have worked as well,
+    we'd replace settings.py in PYTHONPATH with a settings.cfg in PWD.
+    I guess I'm too used to django.
+"""
+
+import os
+import re
+import runpy
+import UserDict
+
+# Uppercase alnum, starts with alpha, underscores between alnums.
+SETTING_RE = re.compile('[A-Z][A-Z0-9]*(_[A-Z0-9]+)*')
+
+class Settings(UserDict.DictMixin, object):
+    """
+    A read-only dictionary that allows access only to properly-named settings.
+
+    Valid settings name are of the form:
+    FOO_BAR_BAZ2
+    """
+
+    def __init__(self, dct):
+        self.__dct = dct
+
+    def __getitem__(self, name):
+        if not SETTING_RE.match(name):
+            # Or maybe ValueError
+            raise KeyError('Invalid name', name)
+        return self.__dct[name]
+
+    def keys(self):
+        return [k for k in self.__dct if SETTING_RE.match(k)]
+
+VIGILO_SETTINGS_MODULE = os.environ.get('VIGILO_SETTINGS_MODULE', 'settings')
+
+settings_raw = runpy.run_module(VIGILO_SETTINGS_MODULE)
+settings = Settings(settings_raw)
+
+def main():
+    from optparse import OptionParser
+    import sys
+
+    parser = OptionParser()
+    parser.add_option('-g', '--get', dest='get', metavar='SETTING_NAME')
+    (opts, args) = parser.parse_args()
+    if opts.get is None:
+        return
+    val = settings[opts.get]
+    sys.stdout.write('%s\n' % val)
+
+if __name__ == '__main__':
+    main()
+
