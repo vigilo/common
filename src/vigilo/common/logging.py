23ee6cc0e07e5f599a0d684330b153c239c61a1c
Gabriel DE PERTHUIS <gabriel.de-perthuis@c-s.fr>
2009-07-24 08:53:04 +0000

    Move out logging integration of twisted and multiprocessing.
    (used the plugin stuff for that)
    
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@364 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- src/vigilo/common/logging.py
+++ src/vigilo/common/logging.py
@@ -6,42 +6,31 @@ Sets up logging, with twisted and multiprocessing integration.
 """
 
 import logging
-import sys
+import runpy
 
-# slight layering issue, move libs to common?
-from vigilo.corr.libs import mp
-import twisted.python.log as twisted_logging
+from vigilo.common.conf import settings
 
 __all__ = ( 'get_logger', )
 
-tw_obs = None
+plugins_loaded = False
 def get_logger(name):
     """
     Gets a logger from a dotted name.
 
-    Ensures early, basic initialisation is done.
     This must replace all uses of logging.getLogger.
+
+    Ensures that plugins listed in settings['LOGGING_PLUGINS']
+    have been registered. Use them for very early logging initialization,
+    such as calls to logging.setLoggerClass or multiprocessing.get_logger .
 
     Since name should be the package name, a common use pattern is:
         LOGGER = get_logger(__name__)
-
-    Limitations: if logging blocks, we block the twisted reactor.
     """
 
-    global tw_obs
-    if tw_obs is None:
-        # This has the side effect of changing the default logger class
-        # to a LoggerAdapter-like that adds processName contextual info.
-        # Must be called early, before this class is instanciated
-        # in getLogger.
-        # This has another side-effect of preparing the muliprocessing
-        # atexit handlers.
-        mp.get_logger()
-        # Propagate from 'multiprocessing' to the parent (the root logger)
-        mp.get_logger().propagate = True
-        # The blocking issue could be addressed by not logging to stderr,
-        # or not directly (a queue could do). Ctrl-S (XOFF) blocks.
-        tw_obs = twisted_logging.PythonLoggingObserver()
-        tw_obs.start()
+    global plugins_loaded
+    if not plugins_loaded:
+        for plugin_name in settings['LOGGING_PLUGINS']:
+            #load_plugin(plugin_name, False) # Needs registry
+            runpy.run_module(plugin_name)['register'](None)
     return logging.getLogger(name)
 
