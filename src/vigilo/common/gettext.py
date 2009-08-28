a1b5a012c1de9341d1471b4358b47e71632a4af1
Thomas BURGUIERE <thomas.burguiere@c-s.fr>
2009-08-28 08:39:22 +0000

    tburguie: ajout de l'internationalisation (i18n)/localisation (l10n)
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@607 b22e2e97-25c9-44ff-b637-2e5ceca36478

---
+++ src/vigilo/common/gettext.py
@@ -0,0 +1,30 @@
+# vim: set fileencoding=utf-8 sw=4 ts=4 et :
+""" VIGILO gettext """
+from __future__ import absolute_import
+
+import gettext
+
+def unchanged(string):
+    """ Fake function to not translate in case traduction file is missing """
+    return string
+
+def translate(module_name):
+    """ 
+    Return function of traduction of the string 
+    passed in parameter (if available) 
+    """
+    # extraction of the module_name from the complete 
+    # module_name (which include the filename we don't want)
+    liste = module_name.strip().split('.')
+    if liste:
+        module_name = liste[0]
+        for i in liste[1:-1]:
+            module_name = module_name + "." + i
+    try :
+        t = gettext.translation(module_name, '/usr/share/locale')
+        translate_ = t.ugettext
+        return translate_
+    except IOError, e :
+        #IOError: [Errno 2] No translation file found for domain: 'vigilo-connector'
+        return unchanged
+
