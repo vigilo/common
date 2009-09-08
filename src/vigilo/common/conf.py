5ad45a436e0eb45bb5f3e3f05d01939b6fe2030c
Aurelien BOMPARD <aurelien.bompard@c-s.fr>
2009-09-08 16:07:16 +0000

    Changement du fonctionnement du module de conf : au lieu de chercher le fichier settings.py dans le PYTHONPATH, il va le chercher successivement dans 3 répertoires :  * /etc/vigilo  * ~/.vigilo  * . (répertoire courant) Le premier répertoire qui contient le fichier est utilisé.
    Il est aussi possible charger un deuxième fichier de configuration, qui sera
    spécifique au module Vigilo concerné, voir la doc d'API pour les détails (mais
    je ferai les premiers appels)
    
    Conséquence directe pour vous : ne plus utilise `PYTHONPATH=../glue` mais soit :
     * utiliser `VIGILO_SETTINGS_MODULE=../glue/settings.py`
     * se déplacer dans le répertoire `glue`
    
    
    
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@667 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- src/vigilo/common/conf.py
+++ src/vigilo/common/conf.py
@@ -1,27 +1,44 @@
 # vim: set fileencoding=utf-8 sw=4 ts=4 et :
-from __future__ import absolute_import
-
-__all__ = ( 'settings', )
-
 """
 A django-inspired settings module.
 
 Use dictionary access on the 'settings' object.
-Define VIGILO_SETTINGS_MODULE in the environment to a dotted module path
+Define VIGILO_SETTINGS_MODULE in the environment to a valid file path
 that will be imported.
-The default is equivalent to 'import settings', meaning you should put
-a settings.py file in the current directory or the PYTHONPATH.
 
-Usage:
+The settings file will be searched in a list of directories defined in
+the L{Settings._dirs} list.
+
+The default file name is "settings.py", and the first valid file is returned.
+
+To load the default config file:
     from vigilo.common.conf import settings
     conntype = settings['MEMCACHE_CONN_TYPE']
+
+To load a module-specific config file:
+    from vigilo.common.conf import settings
+    settings.load("connector")
+
+By loading a module, the filename will be looked for in a subdirectory
+named after the module. Examples:
+
+C{settings.load()} looks for:
+  - /etc/vigilo/settings.py
+  - ~/.vigilo/settings.py
+  - ./settings.py
+
+C{settings.load("connector")} looks for:
+  - /etc/vigilo/connector/settings.py
+  - ~/.vigilo/connector/settings.py
+  - ./connector/settings.py
+
+If you define VIGILO_SETTINGS_MODULE to a valid filename, it will override the
+search mechanism and be used instead.
 
 Command-line usage:
     conntype=$(python -m vigilo.common.conf --get MEMCACHE_CONN_TYPE)
 
-"""
 
-"""
 Impl notes:
     a ConfigParser.SafeConfigParser would have worked as well,
     we'd replace settings.py in PYTHONPATH with a settings.cfg in PWD.
@@ -31,12 +48,14 @@ We can't import any other Vigilo code here.
 This is because Vigilo code may log or require settings for some other reason.
 """
 
+from __future__ import absolute_import
+
 import os
 import re
-import logging
-import runpy
 import sys
 import UserDict
+
+__all__ = ( 'settings', )
 
 # Uppercase alnum, starts with alpha, underscores between alnums.
 SETTING_RE = re.compile('[A-Z][A-Z0-9]*(_[A-Z0-9]+)*')
@@ -47,10 +66,23 @@ class Settings(UserDict.DictMixin, object):
 
     Valid settings name are of the form:
     FOO_BAR_BAZ2
+
+    @ivar _dirs: Directories to try (in order) for the config file
+    @type _dirs: C{list}
+    @ivar filename: Filename to load, defaults to "settings.py"
+    @type filename: C{str}
+    @ivar conf_file: The path of the chosen file (used for logging)
+    @type conf_file: C{str}
     """
 
-    def __init__(self, dct):
-        self.__dct = dct
+    def __init__(self, filename="settings.py"):
+        self.__dct = {}
+        self._dirs = ["/etc/vigilo",
+                      os.path.join(os.environ.get("HOME"), ".vigilo"),
+                      ".",
+                     ]
+        self.filename = filename
+        self.conf_file = None
 
     def __getitem__(self, name):
         if not SETTING_RE.match(name):
@@ -61,10 +93,37 @@ class Settings(UserDict.DictMixin, object):
     def keys(self):
         return [k for k in self.__dct if SETTING_RE.match(k)]
 
-VIGILO_SETTINGS_MODULE = os.environ.get('VIGILO_SETTINGS_MODULE', 'settings')
+    def load(self, module=None):
+        """
+        Load the configuration, optionnaly giving a Vigilo module
 
-settings_raw = runpy.run_module(VIGILO_SETTINGS_MODULE)
-settings = Settings(settings_raw)
+        @param module: a Vigilo module name
+        @type  module: C{str}
+        """
+        self.conf_file = self.find_file(module)
+        if not self.conf_file:
+            return None
+        settings_raw = {}
+        execfile(self.conf_file, settings_raw)
+        self.__dct.update(settings_raw)
+
+    def find_file(self, module=None):
+        """Search the paths for the settings file"""
+        env_file = os.environ.get('VIGILO_SETTINGS_MODULE', None)
+        if env_file and os.path.exists(env_file):
+            return env_file
+        for d in self._dirs:
+            if module:
+                d = os.path.join(d, module)
+            path = os.path.join(d, self.filename)
+            if not os.path.exists(path):
+                continue
+            return path
+        return None
+
+settings = Settings()
+settings.load()
+
 
 def log_initialized():
     """
@@ -74,10 +133,7 @@ def log_initialized():
 
     from vigilo.common.logging import get_logger
     LOGGER = get_logger(__name__)
-    LOGGER.info('Loaded settings module %r from path %r',
-            VIGILO_SETTINGS_MODULE,
-            settings_raw.get('__file__'),
-            )
+    LOGGER.info('Loaded settings from path %s', settings.conf_file)
 
 def main():
     from optparse import OptionParser
