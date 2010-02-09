23f809d2932065e535ef7c9968d4c063b6c15714
Francois POIROTTE <francois.poirotte@c-s.fr>
2010-02-09 16:30:42 +0000

    Ajout des lgs de Twisted.
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@1897 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- src/vigilo/common/logging.py
+++ src/vigilo/common/logging.py
@@ -6,8 +6,14 @@ from __future__ import absolute_import
 
 import logging, logging.config
 import runpy
+import warnings
 
 from vigilo.common.conf import settings, log_initialized
+
+try:
+    from multiprocessing import current_process
+except ImportError:
+    current_process = None
 
 __all__ = ( 'get_logger', )
 
@@ -16,14 +22,45 @@ def get_logger(name):
     """
     Obtient le logger associé à un nom de module qualifié.
 
+    Cette fonction doit être utilisée à la place de C{logging.getLogger}.
     L'argument L{name} correspond généralement au nom du module appelant.
-    Cette méthode est typiquement utilisée ainsi::
+    Cette fonction est typiquement utilisée ainsi::
         LOGGER = get_logger(__name__)
     """
 
     global plugins_loaded
     if not plugins_loaded:
         plugins_loaded = True
+
+        # Si multiprocessing est disponible, on l'utilise
+        # pour obtenir le nom du processus dont provient
+        # le message de log.
+        old_logger_class = logging.getLoggerClass()
+        class MultiprocessingLogger(old_logger_class):
+            def makeRecord(self, *args, **kwargs):
+                record = old_logger_class.makeRecord(self, *args, **kwargs)
+                if not current_process:
+                    record.processName = '???'
+                else:
+                    record.processName = current_process().name
+                return record
+        logging.setLoggerClass(MultiprocessingLogger)
+
+        # Si Twisted est utilisé, on le configure pour transmettre
+        # ses messages de log aux mécanismes classiques de C{logging}.
+        warnings.filterwarnings(
+                'ignore', category=DeprecationWarning, module='^twisted\.')
+        try:
+            import twisted.python.log as twisted_logging
+        except ImportError:
+            pass
+        else:
+            tw_obs = twisted_logging.PythonLoggingObserver()
+            tw_obs.start()
+
+        # On configure les logs depuis le fichier de settings
+        # et on affiche un message pour indiquer quel fichier
+        # de settings a été utilisé.
         logging.config.fileConfig(settings.filename)
         log_initialized()
 
