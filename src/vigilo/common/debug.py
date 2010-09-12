fef8d1e998908183d8eb10bf0913959f8b1c67e8
Aurelien BOMPARD <aurelien.bompard@c-s.fr>
2010-09-12 10:15:46 +0000

    inutilisé
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@4316 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- src/vigilo/common/debug.py
+++ src/vigilo/common/debug.py
@@ -3,16 +3,18 @@
 Debugging helpers.
 
 Usage::
-    from vigilo.common.debug import debug_here
+    from vigilo.common.debug import DEBUG_HERE
     # … later in your code
-    debug_here()
+    DEBUG_HERE()
+
+Pour l'instant, c'est inutilisé.
 
 """
-from __future__ import absolute_import
-
-from IPython.Debugger import Tracer
-
-__all__ = ( 'DEBUG_HERE', )
-
-DEBUG_HERE = Tracer()
+#from __future__ import absolute_import
+#
+#from IPython.Debugger import Tracer
+#
+#__all__ = ( 'DEBUG_HERE', )
+#
+#DEBUG_HERE = Tracer()
 
