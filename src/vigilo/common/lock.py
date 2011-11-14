fd67f830df772ae09986623412449ec52c2caf85
Aurelien BOMPARD <aurelien.bompard@c-s.fr>
2011-11-14 16:28:33 +0100

    Ajout d'un lock sur connector-syncevents (#901)
    Change-Id: I4f2f77b332c52d81afffafc51e268c4be4470d45
    Refs: #901
    Reviewed-on: https://vigilo-dev.si.c-s.fr/review/392
    Tested-by: Build system <qa@vigilo-dev.si.c-s.fr>
    Reviewed-by: Francois POIROTTE <francois.poirotte@c-s.fr>

---
+++ src/vigilo/common/lock.py
@@ -0,0 +1,48 @@
+# vim: set fileencoding=utf-8 sw=4 ts=4 et :
+# Copyright (C) 2006-2011 CS-SI
+# License: GNU GPL v2 <http://www.gnu.org/licenses/gpl-2.0.html>
+
+import sys
+import fcntl
+import atexit
+
+from vigilo.common.logging import get_logger
+LOGGER = get_logger(__name__)
+
+from vigilo.common.gettext import translate
+_ = translate(__name__)
+
+__all__ = ("grab_lock", )
+
+
+def grab_lock(lockfile):
+    """
+    Ajoute un verrou pour empêcher l'exécution simultanée
+    de plusieurs instances de l'application.
+
+    @note: Le verrou ainsi posé est automatiquement détruit
+        lorsque l'exécution se termine.
+    """
+    f = open(lockfile, 'a+')
+    try:
+        LOGGER.debug("Acquiring the lock.")
+        fcntl.flock(f, fcntl.LOCK_EX | fcntl.LOCK_NB)
+    except IOError, e:
+        LOGGER.error(_("Can't obtain lock on lockfile (%(lockfile)s). "
+                       "Application already running ? REASON : %(error)s"),
+                     { 'lockfile': f.name, 'error': e })
+        sys.exit(1)
+
+    # On veut être sûr que le verrou sera supprimé
+    # à l'arrêt de l'application.
+    atexit.register(delete_lock, f)
+
+def delete_lock(f):
+    """
+    Supprime le verrou posé lors du démarrage lorsque l'application se termine.
+
+    @param f: Descripteur de fichier représentant le verrou.
+    """
+    LOGGER.debug("Removing the lock.")
+    fcntl.flock(f, fcntl.LOCK_UN)
+
