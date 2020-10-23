d9cfbc7c6eee435a87e9b89e958ebca9b2983fdf
Francois POIROTTE <francois.poirotte@c-s.fr>
2020-10-23 17:19:21 +0200

    Installation via setuptools avec des susbtitutions
    Ajoute une commande à setuptools qui permet de gérer automatiquement
    des substitutions dans les fichiers et noms de fichiers à partir de
    variables d'environnement.
    
    Change-Id: Ic402658596fbadfc66ccfd06a4f222bd2ff5e2f6

--- src/vigilo/common/commands.py
+++ src/vigilo/common/commands.py
@@ -3,16 +3,169 @@
 # License: GNU GPL v2 <http://www.gnu.org/licenses/gpl-2.0.html>
 
 """
-Outils pour la manipulation de fichiers de traductions (catalogs).
+Commandes supplémentaires pour setuptools.
 """
-import os.path, os
+import re
+import os
+import stat
+import sysconfig
+from tempfile import mkstemp
+
 from distutils import log
 from distutils.cmd import Command
+from distutils.command.install_data import install_data as orig_install_data
 from distutils.errors import DistutilsOptionError
 
 from babel import Locale
 from babel.core import UnknownLocaleError
 from babel.messages.pofile import read_po, write_po
+
+
+class install_data(orig_install_data):
+    """
+    Installe les fichiers du module contenant des données,
+    tout en supportant des variables de substitution dans le chemin
+    du dossier cible, ainsi que dans les fichiers eux-mêmes.
+    """
+    user_options = orig_install_data.user_options + [
+        ('substitute=', None,
+         "variables to substitute with their environment value"),
+    ]
+
+    def initialize_options(self):
+        orig_install_data.initialize_options(self)
+        self._replacements = {}
+        self.substitute = ''
+        self.subst_pat = None
+
+        self.install_lib = None
+        self.install_purelib = None
+        self.install_platlib = None
+        self.install_scripts = None
+        self.install_headers = None
+
+        self._sysconfig = {}
+        for k in ('BINDIR', 'INCLUDEDIR', 'LIBDIR', 'MANDIR', 'SCRIPTDIR'):
+            v = sysconfig.get_config_var(k)
+            if v:
+                self._sysconfig[k] = v
+
+        self._internal_dirs = {
+            'bindir': 'install_scripts',
+            'datadir': 'install_dir',
+            'includedir': 'install_headers',
+            'libdir': 'install_lib',
+            'platlibdir': 'install_platlib',
+            'purelibdir': 'install_purelib',
+        }
+
+    def finalize_options(self):
+        orig_install_data.finalize_options(self)
+        self.set_undefined_options(
+            'install',
+            ('install_headers', 'install_headers'),
+            ('install_lib', 'install_lib'),
+            ('install_platlib', 'install_platlib'),
+            ('install_purelib', 'install_purelib'),
+            ('install_scripts', 'install_scripts'),
+        )
+
+        pattern = ['[A-Z_][A-Z0-9_]*']
+        pattern.extend(self._internal_dirs)
+        subname = re.compile('^(%s)$' % '|'.join(pattern))
+        subst = self.substitute.replace(',', ' ').split()
+        new_subst = set()
+        for s in subst:
+            if s[0] == '@' and s[-1] == '@':
+                s = s[1:-1]
+            if not subname.match(s):
+                raise DistutilsOptionError(
+                    'Substitutions names may only contain '
+                    'the characters: A-Z, 0-9 and "_"')
+            new_subst.add(s)
+        if new_subst:
+            self.subst_pat = re.compile(
+                "@(%s)@" % '|'.join(reversed(sorted(new_subst))))
+
+        data_files = []
+        for f in self.data_files:
+            if isinstance(f, str):
+                data_files.append(self._subst_variables(f))
+            else:
+                data_files.append( (self._subst_variables(f[0]), f[1]) )
+        self.data_files = data_files
+
+    def _subst_replace(self, match):
+        v = match.group(1)
+
+        if v in self._internal_dirs:
+            res = getattr(self, self._internal_dirs[v])
+
+            if not self.root or not res.startswith(self.root):
+                return res
+
+            root = self.root
+            if root.endswith(os.path.sep):
+                root = root[:-len(os.path.sep)]
+
+            res = res[len(root):]
+            return res
+
+        res = os.environ.get(v)
+        if res is not None:
+            return res
+
+        res = self._sysconfig[v]
+        return res
+
+    def _subst_variables(self, content):
+        if not self.subst_pat:
+            return content
+        return self.subst_pat.sub(self._subst_replace, content)
+
+    def copy_file(self, infile, outfile, preserve_mode=1, preserve_times=1,
+                  *args, **kwargs):
+        basename = os.path.basename(infile)
+
+        # If infile is not a template or is simply named ".in", copy it as-is.
+        if basename == '.in' or not basename.endswith('.in'):
+            return orig_install_data.copy_file(
+                self, infile, outfile, preserve_mode, preserve_times,
+                *args, **kwargs)
+
+        # Copy the content of the original file to a temporary one,
+        # while doing the variables' substitution on the fly.
+        # Only small files (< 1 MB) should be used as templates,
+        # since we are reading the whole file in memory here.
+        try:
+            (outfd, tmpfile) = mkstemp()
+        except Exception:
+            raise
+        else:
+            os.close(outfd) # The low-level API is unreliable in Python < 3.5
+                            # when signals are received (see also PEP 475).
+                            # We thus reopen the file using the high-level API.
+            log.info("substituting variables from %(src)s into %(dest)s" %
+                {'src': infile, 'dest': tmpfile})
+            with open(tmpfile, 'wb') as outfd:
+                with open(infile, 'rw') as infd:
+                    outfd.write(self._subst_variables(infd.read(-1)))
+
+            # Take care of preserve_* arguments (see distutils' code).
+            if preserve_mode or preserve_times:
+                st = os.stat(infile)
+                if preserve_times:
+                    os.utime(tmpfile, (st[stat.ST_ATIME], st[stat.ST_MTIME]))
+                if preserve_mode:
+                    os.chmod(tmpfile, stat.S_IMODE(st[stat.ST_MODE]))
+
+            # Finally, copy the temporary file to its final destination.
+            return orig_install_data.copy_file(
+                self, tmpfile, os.path.join(outfile, basename[:-3]),
+                preserve_mode, preserve_times, *args, **kwargs)
+        finally:
+            os.unlink(tmpfile)
+
 
 class identity_catalog(Command):
     """
@@ -35,25 +188,6 @@ class identity_catalog(Command):
          "'<output_dir>/<locale>/LC_MESSAGES/<domain>.po')"),
         ('locale=', 'l',
          'locale of the catalog to "translate"'),
-#        ('no-location', None,
-#         'do not include location comments with filename and line number'),
-#        ('output-file=', 'o',
-#         'name of the output file'),
-#        ('width=', 'w',
-#         'set output line width (default 76)'),
-#        ('no-wrap', None,
-#         'do not break long message lines, longer than the output line width, '
-#         'into several lines'),
-#        ('sort-output', None,
-#         'generate sorted output (default False)'),
-#        ('sort-by-file', None,
-#         'sort output by file location (default False)'),
-    ]
-    boolean_options = [
-#        'no-location',
-#        'no-wrap',
-#        'sort-output',
-#        'sort-by-file',
     ]
 
     def initialize_options(self):
